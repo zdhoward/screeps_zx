6059859301d9ebd18c794d0e19cd2a055d499136
Zach Howard <3210164+zdhoward@users.noreply.github.com>
2025-12-23 18:25:36 -0500

    v2 flattened

--- ext.bootstrap.js
+++ ext.bootstrap.js
@@ -1,8 +1,2 @@
 // ext.bootstrap.js
 const Traveler = require("ext.Traveler");
-
-if (!global.__travelerInstalled) {
-    Traveler.install();
-    global.__travelerInstalled = true;
-    console.log("[BOOT] Traveler installed");
-}
